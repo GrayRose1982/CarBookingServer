24530da128163a18d0785664a9ca8e9752aaf940
ninhhuong <ninhhuongdst@gmail.com>
2017-05-17 18:57:27 +0700

    - implement register and login function

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var account = require('../model/account');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/account/register', function (req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    account.register(email, password, function (found) {
+        console.log(found);
+        res.json(found);
+    });
+});
+
+router.post('/account/login', function (req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    account.login(email, password, function (found) {
+        console.log(found);
+        res.json(found);
+    });
 });
 
 module.exports = router;
